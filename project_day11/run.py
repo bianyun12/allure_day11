a25ce60718ac850672c79bdde1769a2840976469
bian <1425623024@qq.com>
2020-12-10 10:20:23 +0800

    yeui

--- project_day11/run.py
+++ project_day11/run.py
@@ -2,7 +2,5 @@ import pytest
 
 import os
 
-pytest.main(['--alluredir=testresult/test_report',
-            '--reruns','3',
-            '--reruns-delay','2'])
+pytest.main(['--alluredir=testresult/test_report'])
 # os.system("allure serve testresult/test_report")
